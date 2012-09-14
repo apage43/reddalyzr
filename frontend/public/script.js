9f6d72806ff451d604878ab190093fa00405fd64
Aaron Miller <apage43@ninjawhale.com>
2012-09-14 14:19:51 -0700

    Frontend updates
    Added a cubism-based time series view of data collection, cleaned up bar
    chart javascript.

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,23 +1,26 @@
-var h = $("#chart").height() - 30,
-    w = $("#chart").width() + 3;
+//Set up the chart
 
-var toppad = 25;
+var chartheight = $("#chart").height() - 30,
+    chartwidth = $("#chart").width() + 3;
 
 var UTCOffset = new Date().getTimezoneOffset() / 60;
 var HourShift = -UTCOffset;
 
 var chartsvg = d3.select("#chart")
                .append("svg")
-               .attr("width", w)
-               .attr("height", h + 30);
+               .attr("width", chartwidth)
+               .attr("height", chartheight + 30);
 
+// Keep the last data we retrieved so we can redraw the chart without requesting it again
+// (for resize)
 var last_data = undefined;
 
 var colors = colorbrewer.RdYlBu[9];
 colors.reverse();
 
+// Hour of day axis
 var hrax = d3.svg.axis()
-    .scale(d3.scale.linear().range([0, w - 3]).domain([0,24]))
+    .scale(d3.scale.linear().range([0, chartwidth - 3]).domain([0,24]))
     .tickValues([1, 6, 11, 17, 22])
     .tickFormat(function(t) {
         var end = "am";
@@ -25,11 +28,13 @@ var hrax = d3.svg.axis()
         return t + end;
     }).orient("bottom");
 
+// <g> container for hour axis
 var axg = chartsvg.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + h + ")")
+    .attr("transform", "translate(0," + chartheight + ")")
     .call(hrax);
 
+// Karma score color legend
 var legend = chartsvg.append("g");
 
 legend.append("rect")
@@ -49,14 +54,16 @@ legend.attr("class", "legend")
           .attr("width", 10)
           .attr("height", 10);
 
-
 legend.append("text")
       .attr("x", 10)
       .attr("y", 15)
       .text("Karma score, low to high:")
       .style("fill", "#fff");
 
+// Redraw the chart. Transition duration also specified here so it can be
+// set to 0 for redrawing on resize.
 function show_freqs(row, dur) {
+    var toppad = 25; // Pad the top of the char to leave room for the legend and stuff
     last_data = row;
     if(!row) {
         row = { value: { freqs: [], score: [] }}
@@ -81,16 +88,16 @@ function show_freqs(row, dur) {
     var scorescale = d3.scale.quantile()
                         .domain([maxscore, minscore])
                         .range(colors);
-    var scale = d3.scale.linear().range([0, h - toppad]).domain(domain).nice();
+    var scale = d3.scale.linear().range([0, chartheight - toppad]).domain(domain).nice();
 
     var bars = chartsvg.selectAll(".bar").data(freqdata);
 
     function updatebar(el) {
-        return el.attr("width", (w/24) - 3)
+        return el.attr("width", (chartwidth/24) - 3)
                  .attr("x", function(d) {
-                     return d.disphour * (w/24);
+                     return d.disphour * (chartwidth/24);
                  })
-                 .attr("y", function(d) { return h - scale(d.posts); } )
+                 .attr("y", function(d) { return chartheight - scale(d.posts); } )
                  .attr("class", "bar")
                  .style("fill", function(d) {
                      var scaled = scorescale(row.value.score[d.utchour]);
@@ -105,6 +112,39 @@ function show_freqs(row, dur) {
     updatebar(bars.transition().duration(dur));
     bars.exit().remove();
 }
+
+// Resize the chart to fill the window
+function doresize() {
+    var dw = $(window).width();
+    var cw;
+    if(dw <= 990) {
+        cw = dw - 80;
+    } else {
+        cw = dw - 380;
+    }
+
+    var ch = $(window).height() - 140;
+    $("#content").width(cw)
+    $("#content").height(ch)
+    $("#chart").width(cw);
+    $("#chart").height(ch);
+    chartsvg.attr("width", cw);
+    chartsvg.attr("height", ch);
+    chartheight = ch - 30;
+    chartwidth = cw + 3;
+
+    hrax.scale(d3.scale.linear().range([0, chartwidth - 3]).domain([0,24]))
+    axg.attr("transform", "translate(0," + chartheight + ")").call(hrax);
+
+    if(last_data) {
+        show_freqs(last_data, 0);
+    }
+}
+
+$(window).resize(doresize);
+$(doresize());
+
+// Data fetching
 
 function showrdt(reddit, st, en) {
     var viewquery = { stale: "update_after" };
@@ -127,6 +167,8 @@ function showrdt(reddit, st, en) {
     });
 }
 
+// UI/Controls
+
 var routes = {
     '/:reddit/:start/:end': function(rdt, st, en) {
         $("#reddits .selected").removeClass("selected");
@@ -142,6 +184,8 @@ var routes = {
 
 var router = Router(routes);
 
+// Fetch list of reddits and display sorted by amount of
+// data points.
 $.ajax("/api/subrcount", {
     data: { group_level: 1, stale: "update_after" },
     dataType: "json",
@@ -150,18 +194,24 @@ $.ajax("/api/subrcount", {
             $("#reddits ul").html();
             $("#reddits ul").append(
                 "<li><span><a id='r-all' href='#/all'>all reddits</a></span></li>");
+            // Don't show reddits with < 20 data points. There are a lot of them.
             var reddits = _.sortBy(_.filter(data.rows,function(r) { return r.value >= 20 }), function(r) { return -r.value; });
+
             _.each(reddits, function(r, i) {
                 $("#reddits ul").append(
                     "<li><span><a id='r-" + r.key[0] +
                     "' href='#/" + r.key[0] + "/0/6'>" + r.key[0] +
                     "</a> (" + r.value + ")</span></li>");
             });
+
+            // Don't process the hash route until the reddit list has been diplayed, so that
+            // we can highlight the one we're showing.
             router.init('/all');
         }
     }
 });
 
+// Timezone control
 $("#tzctl a").click(function() {
     var tz = $(this).text();
     $("#tzctl .selected").removeClass("selected");
@@ -175,33 +225,4 @@ $("#tzctl a").click(function() {
         show_freqs(last_data, 500);
     }
 });
-
-function doresize() {
-    var dw = $(window).width();
-    var cw;
-    if(dw <= 990) {
-        cw = dw - 80;
-    } else {
-        cw = dw - 380;
-    }
-
-    var ch = $(window).height() - 140;
-    $("#content").width(cw)
-    $("#content").height(ch)
-    $("#chart").width(cw);
-    $("#chart").height(ch);
-    chartsvg.attr("width", cw);
-    chartsvg.attr("height", ch);
-    h = ch - 30;
-    w = cw + 3;
-    hrax.scale(d3.scale.linear().range([0, w - 3]).domain([0,24]))
-    axg.attr("transform", "translate(0," + h + ")").call(hrax);
-    if(last_data) {
-        show_freqs(last_data, 0);
-    }
-}
-
-$(window).resize(doresize);
-$(doresize());
-
 
--- frontend/server.js
+++ frontend/server.js
@@ -3,14 +3,20 @@ var express = require('express'),
     app = express(),
     request = require('request');
 
-//var reddit_ddoc = "http://127.0.0.1:9500/default/_design/reddit";
-var reddit_ddoc = "http://mango-008:8092/reddalyzr/_design/reddit";
+var reddit_ddoc = "http://10.1.3.248:8092/reddalyzr/_design/reddit";
+var reddit_ts_ddoc = "http://10.1.3.248:8092/reddalyzr/_design/reddit_ts";
 
 app.use(express.static('public'));
 
 app.get("/api/:view", function(req, resp) {
     var url_parts = url.parse(req.url, true);
     var proxy_url = reddit_ddoc + "/_view/" + req.params.view + url_parts.search;
+    request(proxy_url).pipe(resp);
+});
+
+app.get("/ts/:view", function(req, resp) {
+    var url_parts = url.parse(req.url, true);
+    var proxy_url = reddit_ts_ddoc + "/_view/" + req.params.view + url_parts.search;
     request(proxy_url).pipe(resp);
 });
 
